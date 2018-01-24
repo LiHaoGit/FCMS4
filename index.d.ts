b08afc27607292897f9d07852f777d82fa39cea3
lihao <dahao@qq.com>
2018-01-24 16:09:21 +0800

    兼容老项目构建

---
+++ index.d.ts
@@ -0,0 +1,207 @@
+// cSpell:words repo captcha fcms upsert
+
+interface KvOption {
+    key: string,
+    value: string
+}
+
+interface FieldMeta {
+    system?: boolean
+    name: string
+    type: string
+    required?: boolean
+    refEntity?: string
+    multiple?: boolean
+    label: string
+    persistType?: string
+    sqlColM?: number
+    inputType?: string
+    noCreate?: boolean
+    noEdit?: boolean
+    fastSearch?: boolean
+    optionsFunc?: string
+    comment?: string
+    inputFunc?: string
+    editReadonly?: boolean
+    notShow?: boolean
+    fileStoreDir?: string
+    showInListPage?: boolean
+    kvOptions?: KvOption[]
+    textOptions?: string[]
+}
+
+interface MongoIndex {
+    name: string
+    fields: string
+    unique?: boolean
+    sparse?: boolean
+    errorMessage: string
+}
+interface EntityMeta {
+    system?: boolean
+    type?: string
+    _version?: number
+    name: string
+    label: string
+    _modifiedOn?: Date
+    db: string
+    dbName?: string
+    idType?: string
+    tableName?: string
+    noPatchSystemFields?: boolean
+    mongoIndexes?: MongoIndex[]
+    iconField?: string
+    digestConfig?: string
+    fieldsForDigest?: string[]
+    editEnhanceFunc?: string
+    noServiceCache?: boolean
+    noCreate?: boolean
+    noDelete?: boolean
+    noEdit?: boolean
+    history?: number
+    singleton?: boolean
+    fields: {[k: string]: FieldMeta}
+}
+
+interface EntityMetaMap {
+    [k: string]: EntityMeta
+}
+
+interface EntityValue {
+    [k: string]: any
+}
+
+interface GenericCriteria {
+    __type?: string
+    relation?: string
+    items?: any[]
+    field?: string
+    operator?: string
+    value?: any
+    [k:string]: any
+}
+
+type MongoCriteria = any
+
+type AnyCriteria = GenericCriteria | MongoCriteria
+
+interface ExecuteContext {
+}
+
+interface UpdateOption {
+    upsert: boolean
+}
+
+interface FindOption {
+    includedFields?: string[]
+    pageSize?: number,
+    withoutTotal?: boolean
+}
+
+interface ListOption {
+    criteria?: GenericCriteria
+    sort?: {[k:string]: number}
+    includedFields?: string[]
+    pageNo?: number
+    pageSize?: number
+    withoutTotal?: boolean
+}
+
+type WebErrorCatcher = ()=>void
+
+interface RouteInfo {
+    urlPrefix: string
+    errorCatcher?: WebErrorCatcher
+    auth?: boolean
+    authEntity?: string
+    action?: string
+    isPage?: boolean
+    [k:string]:any
+}
+
+interface RouteConfig {
+    admin?: boolean
+    auth?: boolean
+    authEntity?: string
+    action?: string
+    isPage?: boolean
+}
+
+interface Extension {
+    aSendSecurityCodeToEmail?: (toEmail: string, code: string) => void
+    aSendSecurityCodeToPhone?: (toPhone: string, code: string) => void
+    aKoaMiddlewareBeforeHandler?: () => void
+    checkPasswordEquals?: (target: string, notSalted: string) => boolean
+    [k: string]: any
+}
+
+interface CreateResult {
+    id: any
+}
+
+type EntityPage = EntityValue[]
+
+interface PagingListResult {
+    pageNo: number
+    pageSize: number
+    total: number
+    page: EntityPage
+}
+
+interface MongoDBConfig {
+    name: string
+    url: string
+}
+interface SSOServerClient {
+    acceptTokenUrl: string
+    key: string
+}
+
+interface SSOServerConfig {
+    clients: {[origin: string]: SSOServerClient}
+}
+
+interface OriginConfigItem {
+    description?: string
+    ssoServer: string
+    defaultCallbackUrl: string
+    ssoKey: string
+}
+
+interface OriginConfig {
+    [origin: string]: OriginConfigItem
+}
+
+interface SubApp {
+    label: string
+    origins: string[]
+}
+
+interface IConfig {
+    metaFile: string
+    serverPort: number
+    serverSocketTimeout: number
+    cookieKey: string
+    serverPugPath: string
+    uploadPath: string
+    httpBodyMaxFieldsSize: number
+    fileDefaultMaxSize: number
+    imageDefaultMaxSize: number
+    sessionExpireAtServer: number
+    usernameFields: string[]
+    mongoDatabases: MongoDBConfig[]
+    redis: any
+    passwordFormat: RegExp
+    fileDir: string
+    fileDownloadPrefix: string
+    cluster?: boolean
+    workerNum?: number
+    sso?: string
+    subApps?: SubApp[]
+    ssoServer: SSOServerConfig
+    originConfigs: OriginConfig
+    logConfigs: any
+    errorCatcher: WebErrorCatcher | null
+    tuningFileDir?: string
+    preprocess: ()=>void
+}
